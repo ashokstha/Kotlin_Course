bcb807604d600acfbd2f7218892d649bee5483d5
akstha007 <cs.ashok007@gmail.com>
2018-09-05 21:01:14 -0500

    fit more fish challenge

--- src/AMS.kt
+++ src/AMS.kt
@@ -15,19 +15,104 @@ fun dayOfWeek() {
     })
 }
 
-fun feedTheFish() {
-    val day = "Tuesday"
-    val food = "pellets"
-    println("Today is $day and the fish eat $food")
+fun canAddFish(
+        tankSize: Double,
+        currentFish: List<Int> = listOf<Int>(),
+        fishSize: Int = 2,
+        hasDecorations: Boolean = true
+): Boolean {
+    var size = tankSize
+    if(hasDecorations){
+        size = size * 0.8
+    }
+
+    var totalLength = 0
+    for(fish: Int in currentFish){
+        totalLength += fish
+    }
+
+    totalLength += fishSize
+    return if(totalLength<=size) true else false
+
+    //elegant solution
+    //return (tankSize * if (hasDecorations) 0.8 else 1.0) >= (currentFish.sum() + fishSize)
 }
 
-fun randomDay(): String{
-    val week = listOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println("Today is $day and the fish eat $food")
+
+    if (shouldChangeWater(day)) {
+        println("Change the water today.")
+    }
+    println(canAddFish(10.0, listOf(3, 3, 3)))
+    println(canAddFish(8.0, listOf(2, 2, 2), hasDecorations = false))
+    println(canAddFish(9.0, listOf(1, 1, 3), 3))
+    println(canAddFish(10.0, listOf(), 7, true))
+}
+
+fun fishFood(day: String): String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Tuesday" -> "pellets"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Saturday" -> "lettuce"
+        "Sunday" -> "plankton"
+        else -> "fasting"
+    }
+}
+
+fun randomDay(): String {
+    val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(7)]
 }
 
+fun getBirthday(): Int {
+    print("Enter your birthday: ")
+    return readLine()?.toIntOrNull() ?: 1
+}
+
+fun getFortune(birthday: Int): String {
+
+    val fortunes = listOf(
+            "You will have a great day!",
+            "Things will go well for you today.",
+            "Enjoy a wonderful day of success.",
+            "Be humble and all will turn out well.",
+            "Today is a good day for exercising restraint.",
+            "Take it easy and enjoy life!",
+            "Treasure your friends because they are your greatest fortune."
+    )
+
+    return when (birthday) {
+        28, 31 -> fortunes[0]
+        in 1..7 -> fortunes[1]
+        else -> fortunes[birthday.rem(fortunes.size)]
+    }
+}
+
+fun shouldChangeWater(
+        day: String,
+        temperature: Int = 22,
+        dirty: Int = 20
+): Boolean {
+    return true;
+}
 
 fun main(args: Array<String>) {
     //dayOfWeek()
     feedTheFish()
+    //println("Your fortune is: ${getFortune()}")
+
+    /*var fortune: String
+    for (i in 1..10) {
+        fortune = getFortune(getBirthday())
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break;
+    }*/
+
+
 }
